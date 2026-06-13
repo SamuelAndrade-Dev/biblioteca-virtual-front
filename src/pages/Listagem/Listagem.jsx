3de79d7121caa5021c9b2d055ce4ebb1e6f648c9
Marcus Williann Neres dos Santos <marcus.williann@iesb.edu.br>
2026-06-13 12:35:46 -0300

    feat: implementa listagem dinâmica consumindo LivroContext, ajustando a issue 6

--- src/pages/Listagem/Listagem.jsx
+++ src/pages/Listagem/Listagem.jsx
@@ -1,9 +1,41 @@
+import React from "react";
+import { useLivro } from "../../contexts/LivroContext";
+
 function Listagem() {
+    const { livros } = useLivro();
+
     return (
-        <>
-            <h1>Tela de Listagem</h1>
-        </>
-    )
+        <div className="listagem-page">
+            <h1>Listagem de Livros</h1>
+
+            {(!livros || livros.length === 0) ? (
+                <p>Nenhum livro cadastrado.</p>
+            ) : (
+                <div className="table-responsive">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Autor</th>
+                                <th>Categoria</th>
+                                <th>Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {livros.map((livro) => (
+                                <tr key={livro.id}>
+                                    <td>{livro.titulo}</td>
+                                    <td>{livro.autor}</td>
+                                    <td>{livro.categoria}</td>
+                                    <td>{livro.quantidade}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Listagem;
