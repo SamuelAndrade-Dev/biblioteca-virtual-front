9e385f6d3c3d37862234a5b776717982a8de05c2
Samuel-Andrade <samuelandrade.spa@gmail.com>
2026-06-13 11:54:30 -0300

    feat: rotas com react-router. Fecha#1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home/Home"
+import Cadastro from "./pages/Cadastro/Cadastro"
+import Listagem from "./pages/Listagem/Listagem";
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/listagem" element={<Listagem />} />
+      </Routes>
+    </>
   )
 }
 
