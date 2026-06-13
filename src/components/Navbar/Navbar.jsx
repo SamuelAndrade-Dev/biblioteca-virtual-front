46988ace64b88ea3765b26f029cc1053a6d809bb
thiagodf37 <thiago.santos2@iesb.edu.br>
2026-06-13 13:51:48 -0300

    feat: cria componente de navegação da biblioteca virtual

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__brand">
+        <span className="navbar__logo">Biblioteca Virtual</span>
+      </div>
+
+      <nav className="navbar__nav" aria-label="Principal">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/cadastro"
+          className={({ isActive }) =>
+            isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+          }
+        >
+          Cadastro
+        </NavLink>
+        <NavLink
+          to="/listagem"
+          className={({ isActive }) =>
+            isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+          }
+        >
+          Listagem
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
